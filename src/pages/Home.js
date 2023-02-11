5a3627246191010f1694da89ed92c16646224625
istvan-halasz <haistvan@gmail.com>
2023-02-11 16:52:47 +0200

    installed react router dom

---
+++ src/pages/Home.js
@@ -0,0 +1,100 @@
+function HomePage() {
+
+  const image1 = require('../assets/img/01.jpg');
+  const image2 = require('../assets/img/02.jpg');
+  const image3 = require('../assets/img/03.jpg');
+  return (
+    <>
+      <section id="scroll">
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
+                <img
+                  className="img-fluid rounded-circle"
+                  src={image1}
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">For those about to rock...</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center">
+            <div className="col-lg-6">
+              <div className="p-5">
+                <img
+                  className="img-fluid rounded-circle"
+                  src={image2}
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="p-5">
+                <h2 className="display-4">We salute you!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
+                <img
+                  className="img-fluid rounded-circle"
+                  src={image3}
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">Let there be rock!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="py-5 bg-black">
+        <div className="container px-5">
+          <p className="m-0 text-center text-white small">
+            Copyright &copy; Your Website 2022
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default HomePage;
